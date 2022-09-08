1871bfb22fcda964c2d88762e7cd05b8da6ef494
Tanuj Khurana <khurana.tanuj@gmail.com>
2022-09-08 12:26:01 -0700

    Where optimizer poc #1

--- phoenix-core/src/main/java/org/apache/phoenix/compile/WhereOptimizer.java
+++ phoenix-core/src/main/java/org/apache/phoenix/compile/WhereOptimizer.java
@@ -30,8 +30,6 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
-import org.apache.phoenix.expression.DelegateExpression;
-import org.apache.phoenix.schema.ValueSchema;
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.query.QueryServicesOptions;
 import org.apache.phoenix.thirdparty.com.google.common.base.Optional;
@@ -148,7 +146,9 @@ public class WhereOptimizer {
             // instead of an array of slots for the corresponding column. Change the behavior so it
             // becomes consistent.
             keySlots = whereClause.accept(visitor);
-    
+            KeySpaceExpressionVisitor ksv = new KeySpaceExpressionVisitor(context, table);
+            KeySpaces keySpace = whereClause.accept(ksv);
+
             if (keySlots == null && (tenantId == null || !table.isMultiTenant()) && table.getViewIndexId() == null && !minOffset.isPresent()) {
                 context.setScanRanges(ScanRanges.EVERYTHING);
                 return whereClause;
